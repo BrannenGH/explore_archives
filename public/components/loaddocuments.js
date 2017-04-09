42e9278d765f1ef4b1cc89fffe49bf7eef0ced07
BrannenGHall <Brannen@HurtadoHall.me>
2017-04-09 12:56:56 -0500

    Fix up HTML

--- public/components/loaddocuments.js
+++ public/components/loaddocuments.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $.each(data, function(key, value){
             for (var i=0; i<10; i++){
                 $.getJSON("/apid/"+value[i],function(data){
-                    var insert = '<div class="container"><div class="row"><div class="col-md-3"><div class="col-md-6 col-xs-12 list-group-item" id="' + data["_id"] + '"><div class="container"><div class="row"><div class="col-xs-3"><h2>'+ data["callnumber"] + '</h2><div hidden id="etc1' + data["_id"] +'"</div></div></div><div class="col-xs-9"><h2>' + data["properties"]["location"] + '</h2><div hidden id="etc2' + data["_id"] +'"<p>' + data["properties"]["details"] + '</p></div></div></div></div></div></div>';
+                    var insert = '<div class="container"><div class="row"><div class="col-xs-1"></div><div class="col-xs-10 list-group-item" id="' + data["_id"] + '"><div class="container"><div class="row"><div class="col-xs-3"><h2>'+ data["callnumber"] + '</h2><div hidden id="etc1' + data["_id"] +'"</div></div></div><div class="col-xs-9"><h2>' + data["properties"]["location"] + '</h2><div hidden id="etc2' + data["_id"] +'"<p style="word-break: break-all;">About: ' + data["properties"]["details"] + '</p><p>Name: ' + data["properties"]["instituionname"] +'</p><p>Head: '+data["properties"]["head"]+'<div class="col-xs-1"></div></div></div></div></div></div>';
                     $(insert).appendTo(".documentview");
                     var documentaddress = data["_id"]
                     document.getElementById(documentaddress).addEventListener("click",function(event){
